75e4cfb4753b62dfc0932b119f0bda8ca4e72c6e
federicaramirez <federicaaramirez@gmail.com>
2022-10-17 15:29:58 +0000

    finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
 
     const [tarea, setTarea] = useState("")
     const [listaTareas, setListaTareas] = useState([])
+    const [listaVacia, setListaVacia] = useState ("No hay tareas")
 
     function subirTarea(e) {
         if (e.key === 'Enter') {
@@ -17,28 +18,34 @@ const Home = () => {
             ]); // suma la nueva sin borrar lo anterior
             console.log(listaTareas);
             setTarea("");
-        }
+            setListaVacia("")
+        } 
 
     }
+
 
     function eliminarElemento(id) {
         let sinEliminado = [];
         sinEliminado = listaTareas.filter((item,index) => {
-            if (index !== id) {
+            if (index !== id)  {
                 return item
             } 
-        } )
+            if (listaVacia == "" && listaTareas.length == 1 ) {
+                setListaVacia("No hay tareas")
+            } 
+        } ) 
         setListaTareas(sinEliminado);
     }
       
 
+
     return (
         <div>
             <div>
-            <h1>Titulo</h1>
+            <h1>Tareas</h1>
             </div>
             <div>
-                <input type="text" className="form-control justify-content-center" id="formGroupExampleInput" placeholder="Example input placeholder"
+                <input type="text" className="form-control justify-content-center" id="formGroupExampleInput" placeholder="Escribir tarea"
                     value={tarea}
                     onChange={
                         (e) => setTarea(e.target.value)
@@ -46,8 +53,14 @@ const Home = () => {
                     onKeyDown={subirTarea}/>
             </div>
                 <ul className="list-group list-group-flush">
-					{listaTareas.map((item, id) => <li className = "list-group-item " key={id}>{item} <button type="button" class="btn btn-outline-primary float-end" onClick={()=> eliminarElemento(id)}>x</button></li> ) }
+					{listaTareas.map((item, id) => 
+                    <li className = "list-group-item hidden" key={id}>{item} 
+                    <button type="button" className="btn btn-outline-primary float-end" 
+                    onClick={()=> eliminarElemento(id)}>x
+                    </button>
+                    </li> ) }
 				</ul>
+                {listaVacia}
 
             </div>
     );
